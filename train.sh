e0841a043a33f6bd706995370ad6eab346243fdc
bendimol <bendimol@uio.no>
2024-02-08 15:27:28 +0100

    updated training script

--- train.sh
+++ train.sh
@@ -3,25 +3,25 @@ set -ex
 NUM_GPUS="${SLURM_GPUS_ON_NODE:-1}"
 NUM_CPUS="$((${SLURM_CPUS_ON_NODE:-8} * 2))"
 TASK_NAME="${1:-Asterix}"
-export CUDA_VISIBLE_DEVICES=0,1
+export CUDA_VISIBLE_DEVICES=1,2
 
 # To set these values, start the program following these instructions:
 #
 #  1. Leave CPU actors at 1, and increase GPU actors until GPU full according
 #     to `ray satus`
 #  2. Increase CPU actors until CPU full.
-GPU_ACTORS=5
-CPU_ACTORS=3
+GPU_ACTORS=6
+CPU_ACTORS=10
 
 #py-spy top --subprocesses -r 10 -- \
-python main.py --env "procgen-chaser-v0" --case procgen --opr train \
-  --num_gpus 2 --num_cpus 12 --gpu_mem 10 \
+python main.py --env "procgen-coinrun-v0" --case procgen --opr train \
+  --num_gpus 2 --num_cpus 16 --gpu_mem 10 \
   --cpu_actor $CPU_ACTORS --gpu_actor $GPU_ACTORS \
   --seed 0 \
   --use_priority \
   --use_max_priority \
   --amp_type 'torch_amp' \
-  --info 'EfficientZero-V3' \
-  --save_video \
+  --info 'MuZero-V0' \
+  --record_video \
   --auto_resume \
   --object_store_mem=1000000000
