ac3d85c0db03ced55f8967cd4e920db4357a01c7
bendimol <bendimol@uio.no>
2024-04-17 12:06:31 +0200

    updated test

--- test.sh
+++ test.sh
@@ -2,9 +2,9 @@ set -ex
 export CUDA_DEVICE_ORDER='PCI_BUS_ID'
 export CUDA_VISIBLE_DEVICES=0
 
-python main.py --env 'ALE/Breakout-v5' --case atari --opr test --seed 0 --num_gpus 1 --num_cpus 20 --force \
-  --test_episodes 32 \
+python main.py --env 'MiniGrid-LavaCrossingS7N1-' --case minigrid --opr test --seed 0 --num_gpus 1 --num_cpus 20 --force \
+  --test_episodes 100 \
   --load_model \
   --amp_type 'torch_amp' \
-  --model_path 'results/atari/EfficientZero-V1/ALE/Breakout-v5/seed=0/model.p' \
+  --model_path 'results/LavaGapS7/MuZero/img=True/av=False/seed=0/model/model_20000.p' \
   --info 'Test'
